a8660968e4f5f3f5ce010f01e26825e0aef75ffc
Chunkes <artmasvic@gmail.com>
2024-07-17 20:47:00 +0300

    2-module 3-task

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,10 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+    let result = {}
+    for (let key in obj) {
+        if (!fields.includes(key))
+        result[key] = obj[key]
+    }
+    return result;
 };
